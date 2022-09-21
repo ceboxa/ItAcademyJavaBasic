9f9f18a125af455da3cbd12c5c5b5ed82f3c4ac6
Sergei Warapai <113501806+ceboxa@users.noreply.github.com>
2022-09-21 11:21:38 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("My name is Sergei");
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a' ;
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
     }
 }
